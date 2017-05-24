6da0ecdc722a3eaca1f3f9e688d241b8975a52e9
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-05-24 08:28:26 -0700

    js lint commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cutstream-api
+
+
+
+cd "C:\Program Files\MongoDB\Server\3.4\bin"
+mongod.exe --dbpath="f:\code\cutstream\api\server\data"
--- server/models/video.js
+++ server/models/video.js
@@ -2,4 +2,8 @@
 
 module.exports = function(Video) {
 
+	Video.validatesPresenceOf(
+		'title', 'url', 'start', 'end', 'step', 'created_at', 'updated_at'
+	);
+
 };
