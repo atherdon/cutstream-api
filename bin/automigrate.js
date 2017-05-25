5ff84aea975e5d1fb5fc83fbbbc32476efbea03e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-05-24 19:30:07 -0700

    3

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -1,1 +1,49 @@
 'use strict';
+
+
+var path   = require('path');
+
+var app    = require(path.resolve(__dirname, '../server/server'));
+var server = app.datasources.videoDS;
+
+server.automigrate('User', function(err) {
+  if (err) throw err;
+
+  var accounts = [
+    {
+      name: 'john',	
+      email: 'john.doe@ibm.com',
+      password: 'john',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+	  name: 'jane',
+      email: 'jane.doe@ibm.com',
+      password: 'jane',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+	  name: 'admin',
+      email: 'jane.doe@ibm.com',
+      password: 'admin',
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+
+  ];
+
+  var count = accounts.length;
+  accounts.forEach(function(account) {
+    app.models.Account.create(account, function(err, model) {
+      if (err) throw err;
+
+      console.log('Created:', model);
+
+      count--;
+      if (count === 0)
+        server.disconnect();
+    });
+  });
+});
