8539deeb7baf83c0b6da486aa49c0e7af3ba7467
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-05-25 18:53:52 -0700

    automigrate

--- bin/automigrate.js
+++ bin/automigrate.js
@@ -6,44 +6,109 @@ var path   = require('path');
 var app    = require(path.resolve(__dirname, '../server/server'));
 var server = app.datasources.videoDS;
 
-server.automigrate('User', function(err) {
-  if (err) throw err;
+// server.automigrate('user', function(err) {
+//   if (err) throw err;
 
-  var accounts = [
-    {
-      name: 'john',	
-      email: 'john.doe@ibm.com',
-      password: 'john',
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
-    {
-	  name: 'jane',
-      email: 'jane.doe@ibm.com',
-      password: 'jane',
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
-    {
-	  name: 'admin',
-      email: 'jane.doe@ibm.com',
-      password: 'admin',
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
+//   var accounts = [
+//     {
+//       name: 'john',	
+//       email: 'john.doe@ibm.com',
+//       password: '$2a$10$1lmPRI0Xjd5fU8HGdPmDoOkZpIPJj2axcdJYIfc/3RUnBDDqQe31K',
+//       created_at: new Date(),
+//       updated_at: new Date(),
+//     },
+//     {
+// 	  name: 'jane',
+//       email: 'jane.doe@ibm.com',
+//       password: '$2a$10$1lmPRI0Xjd5fU8HGdPmDoOkZpIPJj2axcdJYIfc/3RUnBDDqQe31K',
+//       created_at: new Date(),
+//       updated_at: new Date(),
+//     },
+//     {
+// 	  name: 'andy',
+//       email: 'admin@ibm.com',
+//       password: '$2a$10$1lmPRI0Xjd5fU8HGdPmDoOkZpIPJj2axcdJYIfc/3RUnBDDqQe31K', //andy
+//       created_at: new Date(),
+//       updated_at: new Date(),
 
-  ];
+//     },
 
-  var count = accounts.length;
-  accounts.forEach(function(account) {
-    app.models.Account.create(account, function(err, model) {
-      if (err) throw err;
 
-      console.log('Created:', model);
+//   ];
 
-      count--;
-      if (count === 0)
-        server.disconnect();
-    });
-  });
-});
+//   var count = accounts.length;
+//   accounts.forEach(function(account) {
+//     app.models.User.create(account, function(err, model) {
+//       if (err) throw err;
+
+//       console.log('Created:', model);
+
+//       count--;
+//       if (count === 0)
+//         server.disconnect();
+//     });
+//   });
+// });
+
+
+
+// server.automigrate('RoleMapping', function(err) {
+// 	if (err) throw err;
+
+// 	var roleMapping = {
+// 		principalType: "USER",
+// 		principalId: "59278006448ae509a00261c4",
+// 		roleId: '59278362b64ed01098857a4f',
+// 		// id: 1
+// 	};
+
+// 	app.models.RoleMapping.create(roleMapping, function(err, model) {
+// 		if (err) throw err;
+
+// 		console.log('Created:', model);
+
+// 		server.disconnect();
+// 	});
+
+// });
+// 592782bdab75780754df2355
+ 
+// server.automigrate('Role', function(err) {
+// 	if (err) throw err;
+
+// 	var role = {
+// 		name: "admin",
+// 		created_at:"2017-02-21T06:07:25.571Z",
+// 		updated_at:"2017-02-21T06:07:25.571Z",
+// 		// id:1
+// 	};	
+
+// 	app.models.Role.create(role, function(err, model) {
+// 		if (err) throw err;
+
+// 		console.log('Created:', model);
+
+// 		server.disconnect();
+// 	});
+
+// });
+
+// server.automigrate('AccessToken', function(err) {
+// 	if (err) throw err;
+
+// 	var token = {
+// 		"id":"eZRy1Qv7YVx9xEcpLQMl9eruyEIEZd5UECmQOGWYCenAVysIUOCwWQOqLlkY9Gno",
+// 		"ttl":1209600,
+// 		"created":"2017-05-21T22:58:46.468Z",
+// 		"userId": '592642c1e675f021a033ebac'
+// 	};
+
+// });
+
+
+
+// admin id for development instance 592642c1e675f021a033ebac
+ 
+ // "AccessToken": {
+ //      "eZRy1Qv7YVx9xEcpLQMl9eruyEIEZd5UECmQOGWYCenAVysIUOCwWQOqLlkY9Gno": "{\"id\":\"eZRy1Qv7YVx9xEcpLQMl9eruyEIEZd5UECmQOGWYCenAVysIUOCwWQOqLlkY9Gno\",\"ttl\":1209600,\"created\":\"2017-05-21T22:58:46.468Z\",\"userId\":1}"
+ //    },
