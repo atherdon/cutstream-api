45444e7bfd85471770c6ad0f75d5bf99284259f0
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-05-28 11:52:37 -0700

    upd

---
+++ server/models/user-model.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function(Usermodel) {
+
+	Usermodel.validatesPresenceOf('name');
+    Usermodel.validatesLengthOf('password', {min: 5, message: {min: 'Password is too short'}});
+    
+
+	Usermodel.observe("before save", function updateTimestamp(ctx, next) {
+ 
+    	if( ctx.isNewInstance ){
+    		ctx.instance.created_at = new Date();
+    		ctx.instance.updated_at = new Date();
+    	} 
+    	
+    	next();
+  });
+
+  Usermodel.observe('update', function(ctx, next){
+  	ctx.instance.updated_at = new Date();
+  	next();
+  });    
+};
--- server/models/user.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function(User) {
-
-	User.validatesPresenceOf('name');
-    // user.validatesLengthOf('password', {min: 5, message: {min: 'Password is too short'}});
-    // user.validatesUniquenessOf('email', {message: 'email is not unique'});
-};
