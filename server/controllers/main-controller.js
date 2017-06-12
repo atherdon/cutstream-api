62328851ad65ad5deedc6747ba68d9cdb82dbdac
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-06-12 05:41:52 -0700

    upd

--- server/controllers/main-controller.js
+++ server/controllers/main-controller.js
@@ -1,1 +1,82 @@
-main-controller.js
+'use strict';
+
+
+var Video    = require('../models/mongoose/video');
+
+exports.getHomepage = function(req, res, next){
+
+	res.render('index', { title: 'Express' });
+
+};
+
+exports.postVideo = function(req, res, next){
+
+	// console.log(util.inspect( req.body, false, null ));
+
+	//@todo remove startB & endB from form elements
+
+	var url   = req.body.url;
+	
+	var start = req.body.start;
+	var end   = req.body.end;
+	var title = req.body.title;
+	var desc  = req.body.desc;
+	var step  = req.body.step; 
+	
+
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match  = url.match(regExp);
+    var slug   = (match&&match[7].length==11)? match[7] : false;
+
+	var value = new Video({
+	     title : title,
+	     url   : url,
+	     desc  : desc,
+	     start : start,
+	     end   : end,
+	     step  : step,
+	     slug  : slug,
+	});
+
+	// console.log( value );	
+	//@TODO change to bluebird version with async
+// http://stackoverflow.com/questions/25555139/bluebird-promisies-crud-example-using-nodejs-express-and-mongoose
+	value.save().then(function(docs){
+
+		// console.log( docs );
+		// console.log( docs._id );
+
+		docs.linka = '/edit/' + docs._id + ''; //@todo change this later and move to layout template
+		// docs.linka = '/edit/?gid=' + docs._id + '';
+
+		// res.render('player', docs);
+
+
+		res.render('player3', docs);
+
+	});
+
+
+
+};
+
+exports.getExample = function(req, res, next){
+
+	var object = {
+		title    : false,
+		subtitle : false,
+		link     : false,
+		body     : false,
+	}
+
+	res.render('example1', object);
+
+};
+
+//@TODO change to bluebird version with async
+// firstRow.save().then(function(docs){
+
+// 	console.log(docs);
+// 	console.log('Video created!');
+
+// });
--- server/controllers/player-controller.js
+++ server/controllers/player-controller.js
@@ -1,1 +1,103 @@
-player-controller.js
+'use strict';
+
+
+var Video    = require('../models/mongoose/video');
+
+exports.getVideo = function(req, res, next){
+//@TODO change to bluebird version with async
+// Todo.findAsync()
+//      .then(function(todos) {
+//        res.render('todos', {title: 'Todos', todos: todos});
+//      })
+//      .catch(next)
+//      .error(console.error);
+	Video.findById( req.params.id, function(err, video) {
+
+		if (err) res.render('empty', { title: 'Error' });
+				// res.send(err);
+
+			res.render('player-only', video );
+
+	});
+
+
+
+
+};
+
+exports.getVideoById = function(req, res, next){
+
+	// console.log(util.inspect( req.params, false, null ));	
+//@TODO change to bluebird version with async
+// Todo.findAsync()
+//      .then(function(todos) {
+//        res.render('todos', {title: 'Todos', todos: todos});
+//      })
+//      .catch(next)
+//      .error(console.error);
+ 	Video.findById( req.params.id, function(err, video) {
+
+		if (err) 
+			res.render('empty', { title: 'Error' });
+				// res.send(err);
+
+			console.log(video)	;
+
+			// video.duration = req.params.duration;
+
+			res.render('edit', video ); //@todo change to sending maybe two objects, or calculate duration by youtube on page instead
+
+	
+
+			
+
+	});
+
+};
+
+exports.update = function(req, res, next){
+
+	//@todo receive only update form values
+	// console.log(util.inspect( req.params, false, null ));	
+	console.log(util.inspect( req.body, false, null ));
+	console.log( req.body.id );
+
+	const id = req.body.id;
+	//@TODO change to bluebird version with async
+	// http://stackoverflow.com/questions/30915942/how-to-do-a-findoneandupdate-with-bluebird-promises-mongoose
+	Video.findOneAndUpdate({_id:id}, {start: req.body.start, end:req.body.end}, {}, function(err, video){
+		//@todo add thank you for using our service
+		// res.redirect("index"); //@todo test title
+		// res.render('index', { title: 'CutStream | Start & End point for your video' });
+		if (err) 
+			res.render('empty', { title: 'Error' });
+		
+	});
+
+
+ 
+
+};
+
+// @TODO remove when will be tested
+
+// exports.getVideo = function(req, res, next){
+
+//   var form = {},
+//   error = null,
+//   formFlash = req.flash('form'),
+//   errorFlash = req.flash('error');
+
+//   if (formFlash.length) {
+//     form.email = formFlash[0].email;
+//   }
+//   if (errorFlash.length) {
+//     error = errorFlash[0];
+//   }
+//   res.render(req.render, {
+//     form: form,
+//     error: error,
+//     plans: plans
+//   });
+
+// };
