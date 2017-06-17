f5497ce7d39375ae3bf2403fc9ae8a91fe74b8ae
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-06-17 03:46:27 -0700

    1

--- bin/import-samples2.js
+++ bin/import-samples2.js
@@ -8,7 +8,7 @@ let app         = require(path.resolve(__dirname, '../server/server'));
 
 var database    = app.datasources.videoDS;
 
-let accounts    = require(path.resolve(__dirname, 'sample-users-data'));
+let users    = require(path.resolve(__dirname, 'sample-users-data'));
 
 let videos      = require(path.resolve(__dirname, 'sample-videos-data'));
 
@@ -21,14 +21,18 @@ var Video       = app.models.VideoModel;
 module.exports = function(){
 
 	async.parallel({
-		users: async.apply(),
-		videos: async.apply()
+		users: async.apply(createUsers),
+		videos: async.apply(createVideos)
 	}, function(err, results){
 		if( err ) throw err;
 
 		console.log(results);
 		console.log(results.users);
 		console.log(results.videos);
+
+		// attachVideosToUsers(results.users, results.videos, function(err){
+		// 	console.log('>models create sucessfully');
+		// });
 
 	});
 
@@ -38,7 +42,7 @@ function createUsers(cb){
 	database.autoupdate('UserModel', function(err){
 		if (err) return cb(err);
 
-
+		User.create(users, cb);
 	});
 };
 
@@ -46,6 +50,17 @@ function createVideos(cb){
 	database.autoupdate('VideoModel', function(err){
 		if (err) return cb(err);
 
-		
+		Video.create(videos, cb);
 	});
 };
+
+//attaching videos to admin user
+function attachVideosToUsers(users, videos, cb){
+
+	videos.forEach(function(video){
+		video.updateAttribute('userId', users[0].id);
+		console.log(video.userId);
+	});
+
+	// Video.updateAttribute('userId', users[0].id);
+}
