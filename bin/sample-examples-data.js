c2c24053cd039c5acf02a589e6882e211036c267
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-06-20 07:21:52 -0700

    1

--- bin/sample-examples-data.js
+++ bin/sample-examples-data.js
@@ -1,1 +1,49 @@
-sample-examples-data.js
+'use strict';
+
+module.exports = function getSampleData (){
+
+
+ 
+
+
+	var examples = [	
+		{
+		  img   : 'john',	
+		  title : 'john.doe@ibm.com',
+		  url   : 'john1',
+		  description : 'john1',
+		  description2: 'john1',
+		  videos: []
+
+		},
+		{
+		  img   : 'john',	
+		  title : 'john.doe@ibm.com',
+		  url   : 'john1',
+		  description : 'john1',
+		  description2: 'john1',
+		  videos: []
+		},
+		{
+		  img   : 'john',	
+		  title : 'john.doe@ibm.com',
+		  url   : 'john1',
+		  description : 'john1',
+		  description2: 'john1',
+		  videos: []
+
+		},
+		// {
+		//   img   : 'john',	
+		//   title : 'john.doe@ibm.com',
+		//   url   : 'john1',
+		//   description : 'john1',
+		//   description2: 'john1',
+		//   videos: []
+
+		// }		
+  	];
+
+  	return examples;
+  	// return cb(accounts);
+};
