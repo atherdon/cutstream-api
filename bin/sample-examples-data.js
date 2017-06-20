c4cecc4ef788d3dcf234174cff0583afc4f412de
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-06-20 09:02:34 -0700

    1

--- bin/sample-examples-data.js
+++ bin/sample-examples-data.js
@@ -9,7 +9,7 @@ module.exports = function getSampleData (){
 		  title : 'Guardians of the Galaxy - Easter Egg Countdown',
 		  url   : 'https://www.youtube.com/watch?v=WbIPHjHoYaA',
 		  description : '(Video Description) We may not be cashing in on director James Gunn\'s $100K proposal to find the last remaining Easter Egg in Guardians of the Galaxy, but we count down some of the notables RIGHT HERE in our latest installment of Easter Egg Countdown!',
-		  description2: 'john1',
+		  description2: '',
 		  videos: []
 
 		  // description3: '',
@@ -17,11 +17,15 @@ module.exports = function getSampleData (){
 
 		},
 		{
-		  img   : 'john',	
-		  title : 'john.doe@ibm.com',
-		  url   : 'john1',
-		  description : 'john1',
-		  description2: 'john1',
+		  img   : '/images/maxresdefault (1).jpg',	
+		  title : 'Jamie Staples\' PokerStaples Streams',
+		  url   : 'https://www.youtube.com/watch?v=g0uPUJbOYdg',
+		  description : 'Highlight from my first stream after being away, and we made a final table!',
+		  description2: 'Who is Jamie?',
+		  description3: '|Jaime is a Canadian professional poker player from Alberta, Canada who specializes in live streaming of online low-high stakes multi-table tournaments on Twitch.tv'+
+                    '<br /> | Youtube: https://www.youtube.com/channel/UC7tffpR__YXghv3ypgpE7fg' +
+                    '<br /> |Twitch:  https://www.twitch.tv/pokerstaples <br />',
+          description4: 'Some interesting pieces from his video:'          
 		  videos: []
 		},
 		{
