f031c02df6d1a64dd2db2d48b30cf1d95f98cb43
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2017-07-04 20:41:34 -0700

    emails

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -5,9 +5,9 @@ var path     = require('path');
 
 let server  = require(path.resolve(__dirname, '../server'));
 
-var Video   = server.models.VideoModel;
+// var Video   = server.models.VideoModel;
 var User    = server.models.UserModel;
-var Email   = server.models.EmailModel;
+// var Email   = server.models.EmailModel;
 
 
 exports.getVideo = function(req, res, next){
@@ -37,4 +37,55 @@ exports.verified = function(req, res, next){
 
 	res.render('account/verified');
 
+};
+exports.getLogin = function(req, res, next){
+
+	var credentials = [
+		email: 'admin@ibm.com',
+		password: 'admin'
+	];
+
+	res.render('account/login', {
+		email: credentials.email,
+		password: credentials.password
+	});
+
+};
+
+exports.postLogin = function(req, res, next){
+	User.login({
+		email: req.body.email,
+		password: req.body.password
+	}, 'UserModel', function(err, token){
+		if (err) {
+
+	        if(err.details && err.code === 'LOGIN_FAILED_EMAIL_NOT_VERIFIED'){
+
+	          res.render('account/reponseToTriggerEmail', {
+	            title: 'Login failed',
+	            content: err,
+	            redirectToEmail: '/api/users/'+ err.details.userId + '/verify',
+	            redirectTo: '/',
+	            redirectToLinkText: 'Click here',
+	            userId: err.details.userId
+	          });
+
+	        } else {
+
+	          res.render('account/response', {
+	            title: 'Login failed. Wrong username or password',
+	            content: err,
+	            redirectTo: '/',
+	            redirectToLinkText: 'Please login again',
+	          });
+	        }
+	        return;
+		}
+
+		 res.render('home', {
+	        email: req.body.email,
+	        accessToken: token.id,
+	        redirectUrl: '/api/users/change-password?access_token=' + token.id
+	      });
+	});
 };
